7d06b9e1fe992532c4e341ef35f5d95831c149b9
Vlastimil Kalášek <25kali25@gmail.com>
2025-07-13 13:02:39 +0200

    Test message to deploy server from local

--- src/views/home/index.php
+++ src/views/home/index.php
@@ -2,3 +2,7 @@
     <h1 class="text-4xl font-bold text-gray-800 mb-4">Vítejte na Kali-Framework</h1>
     
 </div>
+
+<script>
+    alert("Hello, this is a test alert from the home page!");
+</script>
