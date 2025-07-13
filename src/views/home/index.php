3ca136ed8bd692a0c7f22a25a52ec77ceec06171
Vlastimil Kalášek <25kali25@gmail.com>
2025-07-13 15:11:36 +0200

    Fix formatting in home page index.php by removing unnecessary whitespace

--- src/views/home/index.php
+++ src/views/home/index.php
@@ -1,8 +1,9 @@
 <div class="text-center">
     <h1 class="text-4xl font-bold text-gray-800 mb-4">Vítejte na Kali-Framework</h1>
-    
+
 </div>
 
 <script>
     alert("Hello, this is a test alert from the home page 2!");
+    alert("This is a second alert to test the functionality!");
 </script>
