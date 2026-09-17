2e0ec9226ea653e56d3f3d6c238e32e2bf305a22
Vlastimil Kalášek <25kali25@gmail.com>
2026-09-17 17:56:04 +0200

    test workflow

--- src/views/home/index.php
+++ src/views/home/index.php
@@ -1,5 +1,5 @@
 <div class="text-center">
-    <h1 class="text-4xl font-bold text-white mb-4">Vítejte na Kali-Framework</h1>
+    <h1 class="text-4xl font-bold text-white mb-4">Vítejte na Kali-Framework2</h1>
 
 </div>
 
